36e8f8edb5b122d38648559049e42ed24f3cfdb3
Mohammad Nadeem <nadeemlari@hotmail.com>
2022-12-19 16:52:43 +0530

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+=======
+﻿using BenchmarkDotNet.Running;
+using EffectiveStopWatch;
+
+BenchmarkRunner.Run<ValueStopwatch>();
+>>>>>>> e901e74 (Initial commit)
--- ValueStopwatch.cs
+++ ValueStopwatch.cs
@@ -1,6 +1,44 @@
+<<<<<<< HEAD
 ﻿namespace EffectiveStopWatch;
 
 public class ValueStopwatch
 {
     
+=======
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+
+namespace EffectiveStopWatch;
+
+[MemoryDiagnoser()]
+public class ValueStopwatch
+{
+    [Benchmark]
+    public TimeSpan OldLoop()
+    {
+        var sw = Stopwatch.StartNew();
+        for (var i = 0; i < 500; i++)
+        {
+            DoSomething();
+        }
+
+        return sw.Elapsed;
+    }
+
+    [Benchmark]
+    public TimeSpan NewLoop()
+    {
+        var ts = Stopwatch.GetTimestamp();
+        for (var i = 0; i < 500; i++)
+        {
+            DoSomething();
+        }
+
+        return Stopwatch.GetElapsedTime(ts);
+    }
+
+    private void DoSomething()
+    {
+    }
+>>>>>>> e901e74 (Initial commit)
 }
